d70cd24e002aaefea64a19d378dfd9eef8934371
stephaniesantosss <stephanieps2016@hotmail.com>
2022-04-10 18:27:54 -0300

    Deletando arquivo gitatribute

--- src/aulas/POO.scala
+++
@@ -1,6 +0,0 @@
-package aulas
-
-object POO extends App {
-
-  print("Hello")
-}
---
+++ src/estrutura_de_dados_e_algoritmo/Solution.scala
@@ -0,0 +1,8 @@
+package estrutura_de_dados_e_algoritmo
+
+object Solution {
+  def main(args: Array[String]): Unit = {
+    
+  }
+
+}
